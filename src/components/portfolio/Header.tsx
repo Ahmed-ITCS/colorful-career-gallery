7ff147147c4d1f9a82e512625506cf0e792dab7a
Ahmed-ITCS <ahmedkhawarbs@gmail.com>
2025-03-27 15:54:03 +0300

    commit

--- src/components/portfolio/Header.tsx
+++ src/components/portfolio/Header.tsx
@@ -1,7 +1,7 @@
 
 import { useState, useEffect } from "react";
 import { cn } from "@/lib/utils";
-
+//aefjkha
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
   const [activeSection, setActiveSection] = useState("home");
